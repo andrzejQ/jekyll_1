740f65501e96c02fc1d87bdcb31c63d4a4961b88
Andrzej Kubaszek <kubaszek@prz.edu.pl>
2022-11-10 07:06:11 +0100

    localhost 4001

--- _r.cmd
+++ _r.cmd
@@ -1,6 +1,7 @@
 :: ostatni człon aktulanego foldera %cd% poprzedzony http://localhost/ oraz wywołanie przeglądarki
 @FOR /F "delims=|" %%i IN ("%cd%") DO (
-  start "jekyll-localhost-%%~ni" http://localhost:4000/%%~ni/
-  start "jekyll-www-%%~ni" https://andrzejq.github.io/%%~ni/ )
+  start "jekyll-www-%%~ni" https://andrzejq.github.io/%%~ni/ 
+  start "jekyll-localhost-%%~ni" http://localhost:4001/%%~ni/
+)
 ::uruchomienie lokalnego serwera www
 bundle exec jekyll serve
